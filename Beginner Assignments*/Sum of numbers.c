9e8455b3c431139e9b5793eef486a50c92f03121
Pavithra-S12 <113096428+Pavithra-S12@users.noreply.github.com>
2022-09-23 11:07:39 +0530

    Add files via upload

---
+++ Beginner Assignments*/Sum of numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+
+{
+    
+int n;
+    
+scanf("%d",&n);
+    
+int sum=0;
+    
+for(int i=1;i<=n;i++)
+    
+{
+        
+sum+=i;
+    
+}
+    
+printf("%d",sum);
+    
+return 0;
+
+}
+Footer
+© 2022 GitHub, Inc.
