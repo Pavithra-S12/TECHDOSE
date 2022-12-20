4abfce00c5236189c3b43ac2c2550521d8db49d3
Pavithra-S12 <113096428+Pavithra-S12@users.noreply.github.com>
2022-12-20 10:02:09 +0530

    Create Word Break II.java

---
+++ RECURSION(backtracking)/Word Break II.java
@@ -0,0 +1,44 @@
+class Solution {
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        
+        HashSet<String> str = new LinkedHashSet<String>(wordDict); 
+        boolean [] dp = new boolean[s.length()  + 1]; 
+        HashMap<Integer, List<String>> dpMap = new HashMap<>(); 
+        HashSet<Integer> lengths = new LinkedHashSet<Integer>();
+        for(String s1 : wordDict){
+            lengths.add(s1.length());
+        }
+        dpMap.put(0, new ArrayList<String>()); dpMap.get(0).add("");
+        dp[0] = true;   str.add("");
+        for(int i = 0; i < s.length(); ++i){
+            for(Integer s1 : lengths){
+                if(i+1 < s1) continue;
+                int j = i+1 - (s1);
+                String cur = s.substring(j, i+1);
+                if(dp[j] && str.contains(cur)){
+                    dp[i+1] = true; 
+                }
+            } 
+        }
+        if(!dp[s.length()]) return Collections.emptyList();
+        for(int i = 0; i < s.length(); ++i){
+            for(Integer s1 : lengths){
+                if(i+1 < s1) continue;
+                int j = i+1 - (s1);
+                String cur = s.substring(j, i+1);
+                if(dp[j] && str.contains(cur)){
+                    if(!dpMap.containsKey(i+1)) {
+                        dpMap.put(i+1, new ArrayList<String>());
+                    }
+                    for(String sb : dpMap.get(j)){  
+                         dpMap.get(i+1).add(sb + (sb.isEmpty() ? "" : " ") + cur);
+                    }
+                    
+                }
+            } 
+        }         
+        if(!dpMap.containsKey(s.length())) return Collections.emptyList(); 
+        return dpMap.get(s.length());
+        
+    }
+}
